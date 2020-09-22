d896e780772f0dd63ac0dcadeb6a3115bef6bc82
Max Keybr <maximefim@gmail.com>
2020-09-22 18:05:41 +0700

    feat: add files

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("Hello cupcake");
+function render() {}
+
+let catalog = [];
+
+fetch("http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books")
+  .then((res) => res.json())
+  .then((body) => {
+    catalog = body;
+    render();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
