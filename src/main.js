fb65b366b17a4ccbf61ef9226c25a799efedc0fb
Max Keybr <maximefim@gmail.com>
2020-09-28 17:50:42 +0700

    feat: add webpack

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ let CATALOG = [];
 //http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books
 // server/catalog.json;
 
-fetch("server/catalog.json")
+fetch("./server/catalog.json")
   .then((res) => res.json())
   .then((body) => {
     CATALOG = body;
